1307efd3a0395de19870809ee046aadc60492458
infiniteduck <53666932+martinge17@users.noreply.github.com>
2024-02-17 23:19:21 +0100

    Update codeAnalysis.py prueba 1

--- codeAnalysis.py
+++ codeAnalysis.py
@@ -309,6 +309,7 @@ def run_codee_analysis():
         # EJECUCION EN GITHUB
         #output = subprocess.check_output("./codee-2024.1.1-linux-x86_64/bin/pwreport --config compile_commands.json library/*.c --json", shell=True)
         #output = subprocess.check_output("./codee-2024.1.1-linux-x86_64/bin/pwreport --config compile_commands.json tests/*.c --json", shell=True)
+        output = subprocess.check_output("pwd")
         output = subprocess.check_output("./codee-2024.1.1-linux-x86_64/bin/pwreport --config compile_commands.json programs/fuzz/*.c --json", shell=True)
 
         output_variable = output.decode("utf-8")
