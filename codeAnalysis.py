9e0f99408c6828195be1be15f3c88f464d135e09
manuamest <manuamest@gmail.com>
2024-02-21 23:34:46 +0100

    Test

--- codeAnalysis.py
+++ codeAnalysis.py
@@ -311,8 +311,8 @@ def run_codee_analysis():
 
         # EJECUCION EN GITHUB
         #output = subprocess.check_output("../codee-2024.1.1-linux-x86_64/bin/pwreport --accept-eula --config compile_commands.json library/*.c --json", shell=True)
-        output = subprocess.check_output("../codee-2024.1.1-linux-x86_64/bin/pwreport --accept-eula --config compile_commands.json tests/*.c --json", shell=True)
-        #output = subprocess.check_output("../codee-2024.1.1-linux-x86_64/bin/pwreport --accept-eula --config compile_commands.json programs/fuzz/*.c --json", shell=True)
+        #output = subprocess.check_output("../codee-2024.1.1-linux-x86_64/bin/pwreport --accept-eula --config compile_commands.json tests/*.c --json", shell=True)
+        output = subprocess.check_output("../codee-2024.1.1-linux-x86_64/bin/pwreport --accept-eula --config compile_commands.json programs/fuzz/*.c --json", shell=True)
 
         output_variable = output.decode("utf-8")
         return output_variable
